d09155b55d4dde7f05e574b90d40e3f1430d0a3c
kwon <kckc0608@naver.com>
2023-09-19 10:24:04 +0900

    Week1 과제 제출

---
+++ Week1/GDSC_1st_HW/src/main/java/org/example/Cat.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cat extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("cat says Meow!");
+    }
+}
---
+++ Week1/GDSC_1st_HW/src/main/java/org/example/Dog.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("dog says Woof!");
+    }
+}
